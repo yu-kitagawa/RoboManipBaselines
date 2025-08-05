50dcc49bb14eecf117a93a538f0a079e3449aa5f
Masaki Murooka <m-murooka@aist.go.jp>
2025-08-05 14:39:32 +0900

    [Teleop] Add init_for_relative_command.

--- robo_manip_baselines/teleop/TeleopBase.py
+++ robo_manip_baselines/teleop/TeleopBase.py
@@ -77,6 +77,8 @@ class TeleopPhase(PhaseBase):
     def start(self):
         super().start()
 
+        self.op.init_for_relative_command()
+
         self.op.teleop_time_idx = 0
         print(
             f"[{self.op.__class__.__name__}] Press the 'n' key to finish teleoperation."
@@ -132,6 +134,8 @@ class EndTeleopPhase(PhaseBase):
 class ReplayPhase(PhaseBase):
     def start(self):
         super().start()
+
+        self.op.init_for_relative_command()
 
         self.op.teleop_time_idx = 0
         print(f"[{self.op.__class__.__name__}] Start to replay the log motion.")
@@ -534,17 +538,6 @@ class TeleopBase(ABC):
             )
             world_idx = self.replay_data_manager.get_meta_data("world_idx")
 
-            # Set initial joint position for relative command
-            if not set(self.args.replay_keys).isdisjoint(
-                [DataKey.COMMAND_JOINT_POS_REL, DataKey.COMMAND_EEF_POSE_REL]
-            ):
-                self.motion_manager.set_command_data(
-                    DataKey.COMMAND_JOINT_POS,
-                    self.replay_data_manager.get_single_data(
-                        DataKey.COMMAND_JOINT_POS, 0
-                    ),
-                )
-
         # Reset environment
         self.env.unwrapped.world_random_scale = self.args.world_random_scale
         self.data_manager.setup_env_world(world_idx)
@@ -555,6 +548,21 @@ class TeleopBase(ABC):
 
         # Reset phase manager
         self.phase_manager.reset()
+
+    def init_for_relative_command(self):
+        for key in [
+            DataKey.COMMAND_JOINT_POS_REL,
+            DataKey.COMMAND_GRIPPER_JOINT_POS_REL,
+            DataKey.COMMAND_EEF_POSE_REL,
+        ]:
+            if key not in self.args.replay_keys:
+                continue
+
+            abs_key = DataKey.get_abs_key(key)
+            self.motion_manager.set_command_data(
+                abs_key,
+                self.replay_data_manager.get_single_data(abs_key, 0),
+            )
 
     def record_data(self):
         # Add time
